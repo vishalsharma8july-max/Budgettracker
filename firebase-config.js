68f943c1d2649dd25024ffa7f85e6f2579014f9d
Vishal <Vishalsharma8july@gmail.com>
2026-09-25 12:53:23 +0530

    Add firebase config file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your Firebase project's config.
+// Firebase console → Project settings → General → Your apps → SDK setup and config
+const firebaseConfig = {
+  apiKey: "AIzaSyBC4ABl_pw1WVdAuPb7WmmgAq9uy17awtM",
+  authDomain: "budgettracker-d1541.firebaseapp.com",
+  projectId: "budgettracker-d1541",
+  storageBucket: "budgettracker-d1541.firebasestorage.app",
+  messagingSenderId: "302989557174",
+  appId: "1:302989557174:web:11c55ca64d2d5440181517"
+};
